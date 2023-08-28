8c22aa29ef8ae369aaa896f763f6e13080174c76
Junseo Oh <40460655+outsung@users.noreply.github.com>
2023-08-28 23:32:08 +0900

    Feat: add OBJPreview

--- src/components/canvas/Preview/index.tsx
+++ src/components/canvas/Preview/index.tsx
@@ -1,8 +1,24 @@
 import { useGLTF } from "@react-three/drei";
+import { useLoader } from "@react-three/fiber";
+import { OBJLoader } from "three/examples/jsm/loaders/OBJLoader";
 
 export const PREVIEW_FILE_BASE_PATH = "/models/previewable/";
-const Preview = ({ filePath }: { filePath: string }) => {
+interface PreviewProps {
+  filePath: string;
+}
+const Preview = ({ filePath }: PreviewProps) => {
+  const isObjFile = filePath.endsWith("obj");
+
+  if (isObjFile) {
+    return <OBJPreview filePath={filePath} />;
+  }
+
+  return <GLTFPreview filePath={filePath} />;
+};
+
+function GLTFPreview({ filePath }: PreviewProps) {
   const { scene } = useGLTF(`${PREVIEW_FILE_BASE_PATH}${filePath}`);
+
   return (
     <>
       <group>
@@ -11,6 +27,19 @@ const Preview = ({ filePath }: { filePath: string }) => {
       <ambientLight />
     </>
   );
-};
+}
+
+function OBJPreview({ filePath }: PreviewProps) {
+  const obj = useLoader(OBJLoader, `${PREVIEW_FILE_BASE_PATH}${filePath}`);
+
+  return (
+    <>
+      <group>
+        <primitive object={obj} />
+      </group>
+      <ambientLight />
+    </>
+  );
+}
 
 export default Preview;
